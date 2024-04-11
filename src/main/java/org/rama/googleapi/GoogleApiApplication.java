9be99f4b712f4d380d4ea675e4448055ceb2e525
Rama <sadamramanayudu@gmail.com>
2024-04-11 13:58:15 -0500

    Spring Boot application with Google API

---
+++ src/main/java/org/rama/googleapi/GoogleApiApplication.java
@@ -0,0 +1,13 @@
+package org.rama.googleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rama/googleapi/GoogleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rama.googleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
